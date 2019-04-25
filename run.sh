6935816d678981f1a7210f353ade44503988cab0
kunkka234 <32356342+kunkka234@users.noreply.github.com>
2019-04-25 22:44:51 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,9 +17,7 @@ echo '-----------------'
 
 traindata=$datadir/train1
 # traindata=$datadir/train20
-# traindata=$datadir/train40
 # traindata=$datadir/train60
-# traindata=$datadir/train80
 # traindata=$datadir/train100
 evaldata=$datadir/eval
 
