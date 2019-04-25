875186af3dd06706c8ad3f1b9727ed803822d754
kunkka234 <32356342+kunkka234@users.noreply.github.com>
2019-04-25 13:14:02 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 datadir=/gs/hs0/tga-egliteracy/data/MNIST_Examples
 modeldir=$PWD/model
 
+echo 'start time'
+date
+echo '-----------------'
+
 ########### skipping data download for tsubame using ###############
 ########### data has been save at /gs/hs0/tga-egliteracy/data/MNIST_Examples ##############
 
@@ -18,8 +22,10 @@ echo 'Start training'
 date
 ./train.sh $modeldir $traindata  >& train.log
 
-echo 'Evaluation and plot example'
+echo 'Evaluation'
 date
 ./eval.sh $modeldir $evaldata >& eval.log
 
+echo '------------------'
+echo 'end time'
 date
